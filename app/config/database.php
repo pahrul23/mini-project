ccc803800f8dd5a2ce59722a65b847d8c7c28415
unknown <ahmdphrl1st14@gmail.com>
2023-08-15 20:14:45 +0800

    database

---
+++ app/config/database.php
@@ -0,0 +1,12 @@
+$active_group = 'default';
+$query_builder = true;
+
+$db['default'] = [
+    'DSN'      => '',
+    'hostname' => 'localhost',
+    'username' => 'root',
+    'password' => '',
+    'database' => 'mini_project',
+    'DBDriver' => 'MySQLi'
+
+];
